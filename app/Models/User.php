991544b439e477946e8acfc7c1e711fdce46593e
Md Rayhan Ali <78335557+rayhan2001@users.noreply.github.com>
2025-02-09 17:42:42 +0600

    some changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, HasRoles, HasApiTokens;
+    use HasFactory, HasRoles, HasApiTokens, Notifiable;
 
     protected $guarded = ['id'];
 }
